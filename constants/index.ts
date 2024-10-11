c53a5d41a7857b9b801a7cc55a74fcbfbda6b977
sahillede940 <sahillede940@gmail.com>
2024-10-12 00:26:43 +0530

    connected with render

--- constants/index.ts
+++ constants/index.ts
@@ -1,5 +1,3 @@
 let URL = "";
-URL = "http://127.0.0.1:8000";
-// URL = "https://api.itsmylife.co.in";
-
+URL = process.env.NEXT_PUBLIC_API_URL || "http://127.0.0.1:8000";
 export const API_URL = URL;
